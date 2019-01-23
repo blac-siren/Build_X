45379303531d175460ec1a0fd50852548a068a6e
blac-siren <husnein.zac@gmail.com>
2019-01-23 21:01:02 +0300

    server & client handle multiple request

--- sockets/multi-conn/client.py
+++ sockets/multi-conn/client.py
@@ -19,7 +19,6 @@ def start_connections(host, port, num_conns):
         sock.connect_ex(server_addr)
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
 
-        
         data = types.SimpleNamespace(
             connid=connid,
             msg_total=sum(len(m) for m in messages),
