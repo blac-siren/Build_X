eb67baa00ed0c49f1c394971e0604b8a12d83b5e
blac-siren <husnein.zac@gmail.com>
2019-01-24 12:13:26 +0300

    Enhance class methods

--- sockets/advance/libserver.py
+++ sockets/advance/libserver.py
@@ -14,6 +14,45 @@ class Message:
         self.request = None
         self.response_created = False
 
+    def _set_selector_events_mask(self, mode):
+        """
+        Set selector to listen for events: mode is 'r', 'w', or 'rw'
+        """
+        if mode == "r":
+            events = selectors.EVENT_READ
+        elif mode == "w":
+            events = selectors.EVENT_WRITE
+        elif mode == "rw":
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        else:
+            raise ValueError(f"Invalid events mask mode {repr(mode)}")
+        self.selectors.modify(self.sock, events, data=self)
+
+    def _read(self):
+        try:
+            data = self.sock.recv(4096)
+        except BlockingIOError:
+            # Resource temporarly unavailable (error EWOULDBDBLOCK)
+            pass
+        else:
+            if data:
+                self._recv_buffer += data
+            else:
+                raise RuntimeError("Peer closed")
+
+    def _write(self):
+        if self._send_buffer:
+            print(f"Sending {repr(self._send_buffer)} to {self.addrs}")
+            try:
+                sent = self.sock.send(self._send_buffer)
+            except BlockingIOError:
+                pass
+            else:
+                self._send_buffer = self._send_buffer[sent:]
+                # Close when the buffer is drained. The response has been sent.
+                if sent and not self._send_buffer:
+                    self.close()
+
     def process_events(self, mask):
         if mask & selectors.EVENT_READ:
             self.read()
