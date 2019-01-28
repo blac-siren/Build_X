8eb56f9fcfb396fdb97fe523d3951a108e471a09
blac-siren <husnein.zac@gmail.com>
2019-01-28 14:12:18 +0300

    setup a server connection

---
+++ webserver/wsgi.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Implementation of Webserver Gateway Interface (WSGI)
+"""
+
+import sys
+import socket
+
+
+class WSGIServer:
+
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    request_queue_size = 1
+
+    def __init__(self, server_address):
+        self.listen_socket = listen_socket = socket.socket(
+            self.address_family,
+            self.socket_type,
+        )
+        listen_socket.bind(address)
+        listen_socket.listen(self.request_queue_size)
+        host, port = listen_socket.getsockname()[:2]
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+        self.headers_set = []
+
+        
